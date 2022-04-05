a59b067b1aceab413a1cbce4d190dc5177709547
Leon564 <nleon564.nl@gmail.com>
2022-04-04 23:09:01 -0600

    fix():meme url

--- controllers/memes.js
+++ controllers/memes.js
@@ -2,10 +2,10 @@ const { db } = require('../db/firebase.js');
 
 const addmeme = async (req,res) => {
     if(!req.query.url)return res.status(401).send({error:true, message: 'url parameter is required' });
-    var phrase=req.query.url;
+    var meme=req.query.url;
     var ref = await db.ref("data/memes/count");
     var count = await ref.once("value");
-    await db.ref(`data/memes/${count.val()}`).set(phrase)
+    await db.ref(`data/memes/${count.val()}`).set(meme)
     await db.ref('data/memes/count').set(admin.database.ServerValue.increment(1));
     return res.status(201).send({ message: "sucess , key: " + count })
 
@@ -14,10 +14,10 @@ const addmeme = async (req,res) => {
 const getRandomeme = async (req,res) => {
     var ref = await db.ref("data/memes/count");
     var count = await ref.once("value");
-    var PhRandom = Math.floor(Math.random() * count.val());
-    const eventref = db.ref(`data/memes/${PhRandom}`);
+    var mRandom = Math.floor(Math.random() * count.val());
+    const eventref = db.ref(`data/memes/${mRandom}`);
     const snapshot = await eventref.once('value');
-    return res.status(201).send({phrase:snapshot.val(),status:'ok'})
+    return res.status(201).send({url:snapshot.val(),status:'ok'})
 }
 
 const Delete = async (req,res) => {
